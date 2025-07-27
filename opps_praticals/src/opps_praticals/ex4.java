7838fc00596ebddee77b4581dbd53378c908e2b7
TANNIRU NANDINI <tannirunandini54@gmail.com>
2025-07-27 15:40:18 +0530

    Initial Commit.

---
+++ opps_praticals/src/opps_praticals/ex4.java
@@ -0,0 +1,10 @@
+package opps_praticals;
+
+public class ex4 {
+	public static void main(String[] args) {
+		Integer obj=Integer.valueOf(10);
+		int x=obj;
+		System.out.println("objectcreaions/cached object"+obj);
+		System.out.println("ex4"+x);
+	}
+}
---
+++ opps_praticals/src/opps_praticals/ex5.java
@@ -0,0 +1,10 @@
+package opps_praticals;
+
+public class ex5 {
+	public static void main(String[] args) {
+		int x=10;
+		Integer obj=x;
+		System.out.println("primitivalue of integer is"+x);
+		System.out.println("ex5 obj is "+obj);
+	}
+}
